16adab93b0ad5dff9a35eb0c5602625e4644fd95
Hugo Ferreira <71095540+guhzoide@users.noreply.github.com>
2024-11-27 22:35:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def home():
     return render_template("index.html", assets=assets, diretorio=diretorio)
 
 if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=False)
